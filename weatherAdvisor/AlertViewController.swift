23f93a33f41aab3f3b1be6ef8fab38cf82e8ceed
Jonah Austin <jonahaustin@jonahs-macbook.local>
2015-10-03 11:24:23 -0400

    Set up initial view controllers, pre State objects.

---
+++ weatherAdvisor/AlertViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AlertViewController.swift
+//  weatherAdvisor
+//
+//  Created by Jonah Austin on 10/3/15.
+//  Copyright © 2015 Jonah Austin. All rights reserved.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    var stateName = String()
+    @IBOutlet weak var testLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        testLabel.text = stateName
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
