46ad4554d5a21bebcd89ad641eb6f7ee94618b2b
Jonah Austin <jonahaustin@jonahs-macbook.local>
2015-10-06 10:24:31 -0400

    functional polygon overlay, project completed.

--- weatherAdvisor/AlertDetailsViewController.swift
+++ weatherAdvisor/AlertDetailsViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MapKit
 
-class AlertDetailsViewController: UIViewController {
+class AlertDetailsViewController: UIViewController, MKMapViewDelegate {
 
     var alert: WeatherAlert = WeatherAlert()
     @IBOutlet weak var eventLabel: UILabel!
@@ -19,6 +20,7 @@ class AlertDetailsViewController: UIViewController {
     @IBOutlet weak var severityLabel: UILabel!
     @IBOutlet weak var certaintyLabel: UILabel!
     @IBOutlet weak var linkLabel: UILabel!
+    @IBOutlet weak var polyMap: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,9 +30,67 @@ class AlertDetailsViewController: UIViewController {
         expiresLabel.text = alert.expires
         urgencyLabel.text = (alert.urgency == "" ? "" : "Urgency: ") + alert.urgency
         severityLabel.text = (alert.severity == "" ? "" : "Severity: ") + alert.severity
-        certaintyLabel.text = "Certainty: " + alert.certainty
+        certaintyLabel.text = (alert.certainty == "" ? "" : "Certainty: ") + alert.certainty
         linkLabel.text = "Link: " + alert.link
+        if (alert.points != "") {
+            polyMap.delegate = self
+            configureMapView()
+        }
         // Do any additional setup after loading the view.
+    }
+    
+    func configureMapView() {
+        let coords: [String] = alert.points.componentsSeparatedByString(" ")
+        let centerPoint = findCenterPointFrom(coords)
+        let span = MKCoordinateSpanMake(0.75, 0.75)
+        let region = MKCoordinateRegionMake(centerPoint, span)
+        polyMap.setRegion(region, animated: true)
+        var clCoords: [CLLocationCoordinate2D] = makeClCoordsFrom(coords)
+        let poly = MKPolygon(coordinates: &clCoords, count: coords.count)
+        polyMap.addOverlay(poly)
+//        let location = CLLocationCoordinate2D(latitude: 40.193 , longitude:-85.388)
+//        let span = MKCoordinateSpanMake(0.05, 0.05)
+//        let region = MKCoordinateRegion(center:location, span:span)
+//        mapView.setRegion(region, animated:true)
+    }
+    
+    func findCenterPointFrom(coordinates: [String]) -> CLLocationCoordinate2D {
+        var centerLatitude: Double = 0.0, centerLongitude: Double = 0.0
+//        var topLat: Double, topLong: Double // most positive, its long
+//        var leftLat: Double, leftLong: Double // its lat, most negative
+//        var rightLat: Double, rightLong: Double // its lat, least negative
+//        var bottomLat: Double, bottomLong: Double // least positive, its long
+        for ordPair in coordinates {
+            let latLon: [String] = ordPair.componentsSeparatedByString(",")
+            centerLatitude += Double(latLon[0])!
+            centerLongitude += Double(latLon[1])!
+        }
+        let numPoints: Double = Double(coordinates.count)
+        centerLatitude = centerLatitude / numPoints
+        centerLongitude = centerLongitude / numPoints
+        return CLLocationCoordinate2D(latitude: centerLatitude, longitude: centerLongitude)
+    }
+    
+    func makeClCoordsFrom(pairs: [String]) -> [CLLocationCoordinate2D] {
+        var cLocations: [CLLocationCoordinate2D] = []
+        for ordPair in pairs {
+            var temp: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 1.0, longitude: 2.0)
+            let latLon: [String] = ordPair.componentsSeparatedByString(",")
+            temp.latitude = Double(latLon[0])!
+            temp.longitude = Double(latLon[1])!
+            cLocations.append(temp)
+        }
+        return cLocations
+    }
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolygon {
+            let polygonView = MKPolygonRenderer(overlay: overlay)
+            polygonView.strokeColor = UIColor.magentaColor()
+            polygonView.lineWidth = 2.0
+            return polygonView
+        }
+        return MKOverlayRenderer(overlay: overlay)
     }
     
     override func didReceiveMemoryWarning() {
